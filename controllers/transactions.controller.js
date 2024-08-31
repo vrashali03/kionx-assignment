f4538cc0f4184a9d506551f8886b1a1ce6ec870b
Vrashali Naitam <vrushali2214@gmail.com>
2024-08-31 22:47:24 +0530

    Transaction Model

--- controllers/transactions.controller.js
+++ controllers/transactions.controller.js
@@ -1,5 +1,14 @@
+const axios = require("axios");
+require("dotenv").config();
+
+const APIKEY = process.env.API_KEY;
+
 async function getTransactions(req, res) {
-  res.send("Hello");
+  const response = await axios.get(
+    `https://api.etherscan.io/api?module=account&action=txlist&address=0xce94e5621a5f7068253c42558c147480f38b5e0d&startblock=0&endblock=99999999&page=1&offset=10&sort=asc&apikey=${APIKEY}`
+  );
+  const data = response.data;
+  res.status(200).send(data);
 }
 
 module.exports = {
--- models/transactions.model.js
+++ models/transactions.model.js
@@ -1,3 +1,33 @@
 const mongoose = require("mongoose");
 
-const Schema = mongoose.Schema({});
+const transactionSchema = mongoose.Schema(
+  {
+    blockNumber: Number,
+    blockHash: String,
+    timeStamp: Number,
+    hash: String,
+    nonce: Number,
+    transactionIndex: Number,
+    from: String,
+    to: String,
+    value: Number,
+    gas: Number,
+    gasPrice: Number,
+    input: String,
+    methodId: String,
+    functionName: String,
+    contractAddress: String,
+    cumulativeGasUsed: Number,
+    txreceipt_status: Number,
+    gasUsed: Number,
+    confirmations: Number,
+    isError: Number,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Transaction = mongoose.model("Tracsaction", transactionSchema);
+
+module.exports = Transaction;
